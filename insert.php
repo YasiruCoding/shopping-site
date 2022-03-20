1b6c64a600f381748cb95e31f8a1db51956dc1cd
Yasiru <92869313+Yasiru100@users.noreply.github.com>
2022-03-20 13:30:49 +0530

    history update v2

--- insert.php
+++ insert.php
@@ -2,8 +2,8 @@
 include_once 'index.php';
 if(isset($_POST['submit']))
 {    
-     $sql = "INSERT INTO history (id, product_name, quantity, price)
-     VALUES ('1', 'name','2','200000')";
+     $sql = "INSERT INTO history (product_name, quantity, price)
+     VALUES ('name','2','200000')";
      if (mysqli_query($conn, $sql)) {
         echo "New record has been added successfully !";
      } else {
